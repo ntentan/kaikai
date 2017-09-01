52e250686bb79ebbd76514cb416fb52665a26337
James Ainooson <jainooson@gmail.com>
2017-09-01 18:01:53 -0500

    removing the cache dependency on the container

--- src/Cache.php
+++ src/Cache.php
@@ -3,22 +3,25 @@
 namespace ntentan\kaikai;
 
 use ntentan\utils\Text;
-use ntentan\panie\Container;
 
-class Cache {
+class Cache
+{
 
     private $backend;
 
-    public function __construct(CacheBackendInterface $backend) {
+    public function __construct(CacheBackendInterface $backend)
+    {
         $this->backend = $backend;
     }
 
-    public function write($key, $value) {
+    public function write($key, $value)
+    {
         $this->backend->write($key, $value);
     }
 
-    public function read($key, $notExists = null) {
-        $object =$this->backend->read($key);
+    public function read($key, $notExists = null)
+    {
+        $object = $this->backend->read($key);
         if ($object === false && is_callable($notExists)) {
             $object = $notExists();
             $this->write($key, $object);
@@ -26,17 +29,14 @@ class Cache {
         return $object;
     }
 
-    public function exists($key) {
+    public function exists($key)
+    {
         return $this->backend->exists($key);
     }
 
-    public function delete($key) {
+    public function delete($key)
+    {
         return $this->backend->delete($key);
-    }
-    
-    public static function getBackendClassName($backend) {
-        return '\ntentan\kaikai\backends\\' . Text::ucamelize($backend) . 'Cache';
     }
 
 }
-
